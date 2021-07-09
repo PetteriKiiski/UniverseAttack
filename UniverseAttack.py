2141e21fa7b12f7cf67dd166e00d7987191f3621
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2021-07-08 19:09:04 -0700

    Added Image and some simple code

---
+++ UniverseAttack.py
@@ -0,0 +1,45 @@
+import pygame, sys, random
+from pygame.locals import *
+pygame.init()
+def HomePage():
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+def GamePage():
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+def MenuPage():
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+def RestartLevelPage():
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+def RestartGamePage():
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+def SaveGamePage():
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+def ExitPage():
+	while True:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
